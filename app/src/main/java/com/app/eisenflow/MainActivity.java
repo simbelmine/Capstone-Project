e95bc8a2c630a10153b5766cdc1692ef43f6395d
Svetoslava Delcheva <simbelmine.sve@gmail.com>
2017-12-18 17:35:06 -0800

    feat: add collapsable material calendar view

--- app/src/main/java/com/app/eisenflow/MainActivity.java
+++ app/src/main/java/com/app/eisenflow/MainActivity.java
@@ -1,11 +1,12 @@
 package com.app.eisenflow;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,18 +14,32 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener,
+        AppBarLayout.OnOffsetChangedListener,
+        View.OnClickListener {
     @BindView(R.id.toolbar) Toolbar mToolbar;
     @BindView(R.id.fab) FloatingActionButton mFab;
     @BindView(R.id.drawer_layout) DrawerLayout mDrawer;
     @BindView(R.id.nav_view) NavigationView mNavigationView;
+    @BindView(R.id.app_bar_layout) AppBarLayout mAppBarLayout;
+    @BindView(R.id.toolbar_month_container) LinearLayout mToolbarMonthContainer;
+    @BindView(R.id.toolbar_arrow) ImageView mToolbarArrow;
+    @BindView(R.id.material_calendar_view) MaterialCalendarView mMaterialCalendarView;
 
     private ActionBarDrawerToggle mToggle;
+
+    public enum State {
+        EXPANDED,
+        COLLAPSED
+    }
+    private State mCurrentState = State.COLLAPSED;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         initViews();
+        rotateMonthArrow(false);
     }
 
     private void initViews() {
@@ -52,6 +68,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                         .setAction("Action", null).show();
             }
         });
+        mToolbarMonthContainer.setOnClickListener(this);
+        mAppBarLayout.addOnOffsetChangedListener(this);
     }
 
     @Override
@@ -79,5 +97,33 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        if (verticalOffset == 0) {
+            mCurrentState = State.EXPANDED;
+            rotateMonthArrow(true);
+        } else if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
+            mCurrentState = State.COLLAPSED;
+            rotateMonthArrow(false);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.toolbar_month_container:
+                boolean isExpanded = mCurrentState == State.EXPANDED ? true : false;
+                rotateMonthArrow(isExpanded);
+                isExpanded = !isExpanded;
+                mAppBarLayout.setExpanded(isExpanded, true);
+                break;
+        }
+    }
+
+    private void rotateMonthArrow(boolean isExpanded) {
+        float rotation = isExpanded ? 0 : 180;
+        ViewCompat.animate(mToolbarArrow).rotation(rotation).start();
     }
 }
