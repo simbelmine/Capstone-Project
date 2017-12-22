b4570606767e44ae8475019727afba35a8fbca67
Svetoslava Delcheva <simbelmine.sve@gmail.com>
2017-12-22 14:19:11 -0800

    feat: prepare the base for SingleTaskActivity - create layout for single task

---
+++ app/src/main/java/com/app/eisenflow/activities/SingleTaskActivity.java
@@ -0,0 +1,19 @@
+package com.app.eisenflow.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.app.eisenflow.R;
+
+/**
+ * Created on 12/21/17.
+ */
+
+public class SingleTaskActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_task);
+    }
+}
